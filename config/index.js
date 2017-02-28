18bf1268dae3025081a7ae3ab2f80316a3a894d0
Noor Eddin Amer <nooralasa@gmail.com>
2017-02-28 15:16:57 -0500

    removing config files from remote

--- config/index.js
+++
@@ -1,9 +0,0 @@
-module.exports.connectionString = process.env.DATABASE_URL || undefined;
-
-module.exports.githubClientId = process.env.GITHUB_KEY ? process.env.GITHUB_KEY.trim() : undefined;
-module.exports.githubClientSecret = process.env.GITHUB_SECRET ? process.env.GITHUB_SECRET.trim() : undefined;
-
-module.exports.gitterClientId = process.env.GITTER_KEY ? process.env.GITTER_KEY.trim() : undefined;
-module.exports.gitterClientSecret = process.env.GITTER_SECRET ? process.env.GITTER_SECRET.trim() : undefined;
-
-module.exports.homepageUri = undefined;
